e11b446bc093fc1056c713b3f0ac2975a9c95be8
malk04 <20nastya0457@gmail.com>
2024-07-22 05:20:00 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Context} from "./utils/Context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/react_test">
             <Context>
                 <App/>
             </Context>
